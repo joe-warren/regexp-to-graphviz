8b917d73ca43942bff202e61f706f6bc7137109b
Joe Warren <hungryjoe.warren@gmail.com>
2021-08-07 15:26:40 +0100

    Print any errors to stderr, not stdout

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,10 +11,11 @@ import qualified GraphViz as GV
 import qualified Printer as P
 import qualified RegExp as RE
 import System.Environment (getArgs)
+import System.IO (stderr)
 
 someFunc :: IO ()
 someFunc = do
   args <- fmap T.pack <$> getArgs
   forM_ args $ \a -> case RE.parseRegExp a of
-    Left err -> T.putStr err
+    Left err -> T.hPutStrLn stderr err
     Right regExp -> T.putStrLn . GV.getGraphvizSource . P.graphRegExp $ regExp
